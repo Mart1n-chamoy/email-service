6c5e69f0e8132516a40e1fc61cbf3bcdbe585f91
Mart1n chamoy <2004colussi@gmail.com>
2025-06-24 16:26:29 -0300

    agregado de protos

--- genproto.sh
+++ genproto.sh
@@ -16,6 +16,9 @@
 
 # [START gke_emailservice_genproto]
 
-python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
+#python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
+
+python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. ./protos/demo.proto
+
 
 # [END gke_emailservice_genproto]
